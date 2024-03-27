f88eab144b187ce942fc27e3fcb05b4e7edc26b6
Bakhrul <bakhrulbila@gmail.com>
2024-03-27 11:16:45 +0700

    push

--- app.vue
+++ app.vue
@@ -3,3 +3,12 @@
     <NuxtWelcome />
   </div>
 </template>
+<script setup lang="ts">
+  useSeoMeta({
+    title: 'My Amazing Site',
+    ogTitle: 'My Amazing Site',
+    description: 'This is my amazing site, let me tell you all about it.',
+    ogDescription: 'This is my amazing site, let me tell you all about it.',    
+    twitterCard: 'summary_large_image',
+  })
+</script>
