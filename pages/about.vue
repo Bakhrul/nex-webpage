8d2d0403265c8c154a475f64c5c38c36a1672b95
Bakhrul <bakhrulbila@gmail.com>
2024-03-27 11:21:41 +0700

    push

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        About
+    </div>
+</template>
+<script setup lang="ts">
+
+    useSeoMeta({
+        title: 'My About Site',
+        ogTitle: 'My About Site',
+        description: 'This is my About site, let me tell you all about it.',
+        ogDescription: 'This is my About site, let me tell you all about it.',
+        twitterCard: 'summary_large_image',
+    })
+</script>
